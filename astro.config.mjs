fa352aa1d3996f07048080ccb1dd2ff2a28464f1
Matheus Roichman <matheuslimaroichman06@gmail.com>
2026-05-05 23:08:18 -0300

    fix: replaced astro node adapter with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +9,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
